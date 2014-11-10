d7eda7776e3effdfa28a700a85e290600a77c2b9
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-10 15:41:18 +0100

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,12 @@
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
+from trytond.pool import Pool
+from .guarantee import *
+
+
+def register():
+    Pool.register(
+        Asset,
+        Guarantee,
+        SaleLine,
+        module='asset_guarantee', type_='model')
---
+++ guarantee.py
@@ -0,0 +1,62 @@
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
+from trytond.model import fields
+from trytond.pool import PoolMeta
+from trytond.pyson import Eval
+from trytond.transaction import Transaction
+
+__all__ = ['Asset', 'Guarantee', 'SaleLine']
+__metaclass__ = PoolMeta
+
+
+class Asset:
+    __name__ = 'asset'
+    guarantee_resource = fields.Function(fields.Char('Resource'),
+        'on_change_with_guarantee_resource')
+    guarantee = fields.Many2One('guarantee.guarantee',
+            'Guarantee',
+            context={
+                'document': Eval('guarantee_resource'),
+                },
+            depends=['guarantee_resource'])
+
+    @fields.depends('sale')
+    def on_change_with_guarantee_resource(self, name=None):
+        if self.id:
+            return str(self)
+        return ''
+
+
+class Guarantee:
+    __name__ = 'guarantee.guarantee'
+
+    @classmethod
+    def _get_origin(cls):
+        origins = super(Guarantee, cls)._get_origin()
+        origins.append('asset')
+        return origins
+
+    @staticmethod
+    def default_document():
+        return Transaction().context.get('document')
+
+
+class SaleLine:
+    __name__ = 'sale.line'
+    asset = fields.Many2One('asset', 'Asset')
+
+    @fields.depends('asset')
+    def on_change_with_line_in_guarantee(self, name=None):
+        if self.asset and self.asset.guarantee:
+            self.guarantee = self.asset.guarantee
+        return super(SaleLine, self).on_change_with_line_in_guarantee(name)
+
+    @fields.depends('asset', methods=['guarantee'])
+    def on_change_asset(self):
+        changes = {}
+        if self.asset and self.asset.guarantee:
+            changes['guarantee'] = self.asset.guarantee.id
+            changes['guarantee.rec_name'] = self.asset.guarantee.rec_name
+            self.guarantee = self.asset.guarantee
+            changes.update(self.on_change_guarantee())
+        return changes
